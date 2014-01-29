c3fa242295cfc1c4197719b95392dac4e1f2339f
RobPaxHomes <robpaxhomes@gmail.com>
2014-01-29 18:24:10 -0500

    Stubbed out ProcessInput file for everyone.

---
+++ CheesyAI/src/com/robpaxhomes/cheesyai/common/IProcessInput.java
@@ -0,0 +1,7 @@
+package com.robpaxhomes.cheesyai.common;
+
+public interface IProcessInput {
+	
+	String processStringInput(String strToProcess);
+
+}
---
+++ CheesyAI/src/com/robpaxhomes/cheesyai/jamie/ProcessInput.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.robpaxhomes.cheesyai.jamie;
+
+import com.robpaxhomes.cheesyai.common.IProcessInput;
+
+/**
+ * @author rbelshaw
+ *
+ */
+public class ProcessInput implements IProcessInput {
+
+	/* (non-Javadoc)
+	 * @see com.robpaxhomes.cheesyai.common.IProcessInput#processStringInput(java.lang.String)
+	 */
+	@Override
+	public String processStringInput(String strToProcess) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
