057ed19a50127e642ccf6e28f6ad473ad8ff41df
RobPaxHomes <robpaxhomes@gmail.com>
2014-01-29 19:46:46 -0500

    Added 'Type something else.' to some of the default implementations to remind us to enter something else.

--- CheesyAI/src/com/robpaxhomes/cheesyai/nick/ProcessInput.java
+++ CheesyAI/src/com/robpaxhomes/cheesyai/nick/ProcessInput.java
@@ -17,7 +17,7 @@ public class ProcessInput implements IProcessInput {
 	@Override
 	public String processStringInput(String strToProcess) {
 		// TODO Auto-generated method stub
-		return new String("Nick has not yet implemented...");
+		return new String("Nick has not yet implemented...Type something else.");
 	}
 
 }
